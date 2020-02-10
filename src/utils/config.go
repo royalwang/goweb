c688a0c18b39f179fc1e1d10efa6bd3591c50e21
royalwang <hxroyal@gmail.com>
2020-02-10 14:40:45 +0800

    更新数据库连接

--- src/utils/config.go
+++ src/utils/config.go
@@ -2,5 +2,5 @@ package utils
 
 const (
 	// SQLDB is mysql
-	SQLDB = "root:h3m0ni6ha8on4iu2bi@tcp(192.168.111.149:3306)/iissy?charset=utf8"
+	SQLDB = "root:Pigs34@Touch@tcp(192.168.3.17:3306)/iissy?charset=utf8"
 )
