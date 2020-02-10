f27cfd7f6ff4354100567725c91435b69c844003
royalwang <hxroyal@gmail.com>
2020-02-10 22:32:20 +0800

    update sql quer used gorm

--- src/access/home.go
+++ src/access/home.go
@@ -1,13 +1,31 @@
 package access
 
 import (
+	"github.com/davecgh/go-spew/spew"
 	"iissy.com/src/models"
 	"iissy.com/src/utils"
 )
 
 // Index is yes.
+//func Index() (*models.Course, error) {
+//	list, err := db.Query("select ID,Subject,Picture,Description from Article order by AddDate desc limit ?", 30)
+//	utils.CheckErr(err)
+//	course := models.Course{}
+//	course.ArticleItems = []*models.Article{}
+//	for list.Next() {
+//		var article models.Article
+//		err = list.Scan(&article.ID, &article.Subject, &article.Picture, &article.Description)
+//		utils.CheckErr(err)
+//
+//		course.ArticleItems = append(course.ArticleItems, &article)
+//	}
+//
+//	list.Close()
+//	return &course, nil
+//}
+
 func Index() (*models.Course, error) {
-	list, err := db.Query("select ID,Subject,Picture,Description from Article order by AddDate desc limit ?", 30)
+	list, err := db2.Raw("select `ID`, `Subject`, `Picture`, `Description` from `Article` order by `AddDate` desc limit ?", 30).Rows()
 	utils.CheckErr(err)
 	course := models.Course{}
 	course.ArticleItems = []*models.Article{}
@@ -18,6 +36,8 @@ func Index() (*models.Course, error) {
 
 		course.ArticleItems = append(course.ArticleItems, &article)
 	}
+
+	spew.Dump(course)
 
 	list.Close()
 	return &course, nil
